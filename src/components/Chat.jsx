c997dca9b167ee67b1e0918ec6dabef9610e1dea
chris_yooo <hhxtreams@gmail.com>
2022-07-22 18:18:57 +0200

    use nano id in array

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,15 +1,41 @@
+import {nanoid} from 'nanoid';
+
 export default function Chat() {
-  const Message = Messages.map(Message => <p key={Message.toString()}>{Message}</p>);
+  const Message = Messages.map(Message => <p key={Message.msg}>{Message.msg}</p>);
   return Message;
 }
 
 const Messages = [
-  'Hey Moin was geht :D',
-  'Samma das geht aber auch auf Bayrisch oder ?',
-  'Vui spernzaln do mehra do legst di nieda, eam sog i. Ham samma ja, wo samma denn dahoam aasgem, Mongdratzal. Measi glei Gschicht, Schaung kost nix Gamsbart Maßkruag da, hog di hi!',
-  'Ja und noch mehr oder ?',
-  'A bissal wos gehd ollaweil Semmlkneedl do dahoam in da. Maderln nackata zua Kaiwe hogg di hera gwiss a bissal, umananda Biawambn damischa! Do owe Kirwa sog i, Gstanzl!',
-  'Und noch was ?',
-  'Schbozal obandeln anbandeln wolpern großherzig barfuaßat, gscheckate wuid: Mechad a hawadere midananda ozapfa heitzdog jo mei is des schee Woibbadinga lem und lem lossn, i moan scho aa a bissal. Do Hetschapfah nomoi Ohrwaschl, ?',
-  'Bavaria ipsum dolor sit amet Deandlgwand Gams nomoi pfundig, wolln blärrd dahoam hob. Milli i daad nomoi, is. Dahoam allerweil glacht, Biazelt so Maderln Weibaleid.',
+  {
+    id: {nanoid},
+    msg: 'Hey Moin was geht :D',
+  },
+  {
+    id: {nanoid},
+    msg: 'Samma das geht aber auch auf Bayrisch oder ?',
+  },
+  {
+    id: {nanoid},
+    msg: 'Vui spernzaln do mehra do legst di nieda, eam sog i. Ham samma ja, wo samma denn dahoam aasgem, Mongdratzal. Measi glei Gschicht, Schaung kost nix Gamsbart Maßkruag da, hog di hi!',
+  },
+  {
+    id: {nanoid},
+    msg: 'Ja und noch mehr oder ?',
+  },
+  {
+    id: {nanoid},
+    msg: 'A bissal wos gehd ollaweil Semmlkneedl do dahoam in da. Maderln nackata zua Kaiwe hogg di hera gwiss a bissal, umananda Biawambn damischa! Do owe Kirwa sog i, Gstanzl!',
+  },
+  {
+    id: {nanoid},
+    msg: 'Und noch was ?',
+  },
+  {
+    id: {nanoid},
+    msg: 'Schbozal obandeln anbandeln wolpern großherzig barfuaßat, gscheckate wuid: Mechad a hawadere midananda ozapfa heitzdog jo mei is des schee Woibbadinga lem und lem lossn, i moan scho aa a bissal. Do Hetschapfah nomoi Ohrwaschl, ?',
+  },
+  {
+    id: {nanoid},
+    msg: 'Bavaria ipsum dolor sit amet Deandlgwand Gams nomoi pfundig, wolln blärrd dahoam hob. Milli i daad nomoi, is. Dahoam allerweil glacht, Biazelt so Maderln Weibaleid.',
+  },
 ];
