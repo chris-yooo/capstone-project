3c28d83fd7999eb6ea894c151adb7a5a7a99975f
chris_yooo <hhxtreams@gmail.com>
2022-07-27 10:35:41 +0200

    remove button

--- src/components/Button.stories.js
+++
@@ -1,9 +0,0 @@
-import Button from './Button';
-
-export default {title: 'Button', component: Button};
-
-export const Default = () => (
-  <InputButton type="submit" id="sendMessage">
-    <Icon icon="carbon:send-alt" style={{fontSize: '28px'}} />
-  </InputButton>
-);
