8e433b420f93c43d17504eb00ff0dbaeb07785fd
chris_yooo <hhxtreams@gmail.com>
2022-08-11 17:17:10 +0200

    changes

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,8 +1,5 @@
 export default function Chat({messages, goToScrollDown}) {
-  console.log(messages);
-
   return messages?.map(message => {
-    // console.log(message);
     return (
       <p key={message._id}>
         {message.msg}
